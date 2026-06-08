e1773d0af960c7a359e6b8f24fd5116b10c845c4
stewardphil <109634348+stewardphil@users.noreply.github.com>
2026-06-08 10:20:36 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const http = require("http");
+const axios = require("axios");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+let lastId = 0;
+
+async function checkSignal() {
+
+    try {
+
+        const response = await axios.get(
+            "https://bestforextradingsetups.dpdns.org/mazikinosz/"
+        );
+
+        const signal = response.data;
+
+        if(signal.id != lastId){
+
+            lastId = signal.id;
+
+            io.emit(
+                "signal",
+                signal
+            );
+
+            console.log(
+                "New Signal:",
+                signal.id
+            );
+        }
+
+    } catch(err){
+
+        console.log(err.message);
+
+    }
+
+}
+
+setInterval(checkSignal, 5000);
+
+app.get("/", (req,res)=>{
+    res.send("Socket Server Running");
+});
+
+server.listen(
+    process.env.PORT || 3000,
+    ()=>{
+        console.log("Running");
+    }
+);
